4ba28fd40947683195e1b32923b5ec842067c6e1
Andrea Vitaletti <vitaletti@dis.uniroma1.it>
2015-01-20 15:19:59 +0100

    fatto

--- U6_8_channels_new.py
+++ U6_8_channels_new.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import struct
 import traceback
 import ConfigParser
+import os
 
 ################################################################################
 ## U6
@@ -34,12 +35,17 @@ file_report_name = start.strftime("%Y-%m-%d-%H-%M-%S")+'.rep'
 
 print "CURRENT DATA FILE IS:"+file_data_name
 
-fout = open(file_data_name, "w")
+folder = Config.get('ExperimentalSetUp','folder')
+
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+fout = open(folder+'/'+file_data_name, "w")
 
 #Copy the config file into the experiment report file
 #shutil.copyfile(CONF_FILE, file_report_name)
 
-repfile = open(file_report_name,'w')
+repfile = open(folder+'/'+file_report_name,'w')
 
 Config.add_section('Event')
 Config.set('Event','Started',start.strftime("%Y-%m-%d-%H-%M-%S"))
@@ -105,7 +111,7 @@ try:
 				chunk = chunk + 1
 				Config.set('Event','Chunk '+str(chunk)+' start',new_start.strftime("%Y-%m-%d-%H-%M-%S"))
 				file_data_name = start.strftime("%Y-%m-%d-%H-%M-%S")+'_CHUNK_'+str(chunk)+'.dat'
-				fout = open(file_data_name, "w")
+				fout = open(folder+'/'+file_data_name, "w")
             
             if r['errors'] != 0:
                 print "Error: %s ; " % r['errors'], datetime.now()
